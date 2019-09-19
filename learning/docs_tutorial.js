4d4aaf1fcaad32c5c37e03e99134542e2d9cbda1
ngancitano <ngancitano@gmail.com>
2019-09-18 21:23:48 -0400

    start thinking-in-react

--- learning/docs_tutorial.js
+++ learning/docs_tutorial.js
@@ -1065,3 +1065,173 @@ class Reservation extends React.Component {
 //
 //      Often, several components need to reflect the same data. In this case, it is smart to 
 //        'lift up' the data to the closest common ancestor. For example:
+//
+//    Consider the following:
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The water will boil.</p>
+    } else {
+        return <p>The water won't boil.</p>
+    }
+}
+
+// the below will render a form and a text output based on the form input
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {temperature: ''};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({temperature: e.target.value});
+    }
+
+    render() {
+        let temperature = this.state.temperature;
+        return (
+            <fieldset>
+                <legend>Enter temp in Celsius:</legend>
+                <input value={temperature}
+                    onChange={this.handleChange} />
+                <BoilingVerdict 
+                    celsius={parseFloat(temperature)}/>
+            </fieldset>
+        );
+    }
+}
+
+
+//
+//
+// Adding a 2nd Input
+let scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
+  
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+      return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {temperature: ''};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.props.onTemperatureChange(e.target.value);
+    }
+
+    render() {
+        let temperature = this.props.temperature;
+        let scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temp in {scaleNames[scale]}</legend>
+                <input value={temperature}
+                    onChange={this.handleChange}/>
+            </fieldset>
+        );
+    }
+}
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+        this.state = {temperature: '', scale: 'c'};
+    }
+
+    handleCelsiusChange(temperature) {
+        this.setState({scale: 'c', temperature});
+    }
+    
+    handleFahrenheitChange(temperature) {
+        this.setState({scale: 'f', temperature});
+    }
+
+    render() {
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+        return (
+            <div>
+            <TemperatureInput scale="c" 
+                temperature={celsius}
+                onTemperatureChange={this.handleCelsiusChange}
+                />
+            <TemperatureInput scale="f" 
+                temperature={fahrenheit}
+                onTemperatureChange={this.handleFahrenheitChange}
+                />
+
+            <BoilingVerdict celsius={parseFloat(celsius)} />
+            </div>
+        );
+    }
+}
+
+
+
+
+
+//
+//
+//
+// Ch 11: Composition vs Inheritance
+//        https://reactjs.org/docs/composition-vs-inheritance.html
+//
+//  Containment:
+//      Some components don't know their children ahead of time, especially
+//        if they're generic 'boxes' such as a sidebar or dialog
+function FancyBorder(props) {
+    return (
+      <div className={'FancyBorder FancyBorder-' + props.color}>
+        {props.children}
+      </div>
+    );
+}
+//
+//  This allows other components to pass arbitrary children to them by JSX nesting...
+// 
+function WelcomeDialog() {
+    return (
+      <FancyBorder color="blue">
+        <h1 className="Dialog-title">
+          Welcome
+        </h1>
+        <p className="Dialog-message">
+          Thank you for visiting our spacecraft!
+        </p>
+      </FancyBorder>
+    );
+}
+//  In the above, <FancyBorder> children </FancyBorder>
+
+// There's alot more on this topic that is beyond the scope of this learning
+
+
+
+
+//
+//
+// Ch 12: Thinking in React
